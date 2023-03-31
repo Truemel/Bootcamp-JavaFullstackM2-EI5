2d8f473dec63e98a1fe60e71e822fae67b2fdcfc
Truemel <tgmelipilla@gmail.com>
2023-03-31 19:52:12 -0300

    Second Commit

---
+++ scriptjavafechas.js
@@ -0,0 +1,13 @@
+function mostrarFecha(){
+    var fecha = new Date();
+    var falta = (new Date(fecha.getFullYear()+1, 0, 1)).getTime()-fecha.getTime();
+    alert(fecha.getTime()+"\n"+(new Date(fecha.getFullYear()+1, 0, 1)).getTime()+"\n"+falta);
+    var dias = falta/(60000*60*24);
+    falta = falta%(60000*60*24);
+    var minutos = falta/60000;
+    falta = falta%60000;
+    var segundos = falta/1000;
+    var dia = ["Domingo", "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado"];
+    alert("hoy es "+dia[fecha.getDay()]+" "+fecha.getDate()+" de "+fecha.getMonth()+" de "+fecha.getFullYear()+", y son las "+fecha.getHours()+" horas, "+fecha.getMinutes()+" minutos con "+fecha.getSeconds()+" segundos.");
+    document.getElementById("tiemporestante").innerHTML = "Falta "+dias+" días, "+minutos+" minutos y "+segundos+" segundos para que termine el año.";
+}
